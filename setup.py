0d20e720bbaf18732f5bf6f81ab76654916a4d39
Viktor Marinho <56888067+viktormarinho@users.noreply.github.com>
2022-06-08 13:51:40 -0300

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,15 +5,15 @@ with open("README.md", "r", encoding='utf-8') as f:
 
 setuptools.setup(
     name='sklORM',
-    version='0.1',
-    scripts=['skl.py'],
+    version='0.1.1',
+    scripts=[],
     author='Viktor Marinho',
     author_email='viktormpcs@gmail.com',
     description='A independent and simple sqlite3 ORM python module',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url='https://github.com/flakevine/skl',
-    packages=setuptools.find_packages(),
+    packages=['skl'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
