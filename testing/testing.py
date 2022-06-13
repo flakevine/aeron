6cde19bfc3496831107e38b2023e683a694209e9
Viktor Marinho <56888067+viktormarinho@users.noreply.github.com>
2022-06-13 16:25:43 -0300

    Update testing.py

--- testing/testing.py
+++ testing/testing.py
@@ -1,3 +1,16 @@
-from skl.orm import Database, Table
+from skl.orm import connect
 
-db = Database(insta_scaffold=True)
+# usuarios_fieldlist = [Field(field_name="Id", field_type="INTEGER", is_primary_key=True),
+#                       Field(field_name="Nome", field_type="TEXT", field_max_length=100),
+#                       Field(field_name="Idade", field_type="INTEGER")]
+#
+# usuarios = Table(table_name="Usuarios", fields=usuarios_fieldlist)
+# db = Database(tables=[usuarios], insta_scaffold=True)
+
+db = connect('database.sqlite')
+
+index = db.get_table_index("Usuarios")
+
+fieldnames = db.get_table_fieldnames(index)
+
+print(db.delete_one_tuple(table_index=index, tuple_id=3))
